8d6a055b008da810df73d924b069babf351882d4
pecherska <97174413+pecherska@users.noreply.github.com>
2023-08-20 15:05:40 +0000

    Deploying to gh-pages from @ pecherska/js@68ad17474bb45f86ebd75b2b1eed5ce7a70da5f7 🚀

---
+++ index.8994a7a7.js
@@ -0,0 +1,2 @@
+console.dir(class{getItems(){return this.items}addItem(s){this.items.push(s)}removeItem(s){const t=this.items.indexOf(s);this.items.splice(t,1)}constructor(s=[]){this.items=s}});
+//# sourceMappingURL=index.8994a7a7.js.map
